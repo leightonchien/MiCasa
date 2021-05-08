f95109d293de620711ff0181c3a8a45de1629b41
ban-mazin <ban.qaddoori89@gmail.com>
2021-05-08 19:26:50 +0000

    add users data

---
+++ express-back-end/db/seeds/users.sql
@@ -0,0 +1,24 @@
+
+VALUES ('Andy Lindsay' , 'andy@gmail.com', 'password', true);
+INSERT INTO users (full_name, email, password, isProfileVisible)
+VALUES ('Gary J' , 'gary@gmail.com', 'password', true);
+INSERT INTO users (full_name, email, password, isProfileVisible)
+VALUES ('Bradley Fung' , 'bradley@gmail.com', 'password', true);
+INSERT INTO users (full_name, email, password, isProfileVisible)
+VALUES ('Sydania' , 'sydania@example.com', 'password', true);
+INSERT INTO users (full_name, email, password, isProfileVisible)
+VALUES ('asma' , 'asma@example.com', 'password', true);
+INSERT INTO users (full_name, email, password, isProfileVisible)
+VALUES ('James Kim' , 'james@gmail.com', 'password', false);
+INSERT INTO users (full_name, email, password, isProfileVisible)
+VALUES ('Jean Hogan' , 'jean@gmail.com', 'password', false);
+INSERT INTO users (full_name, email, password, isProfileVisible)
+VALUES ('Raymond Russell' , 'raymond@gmail.com.com', 'password', true);
+INSERT INTO users (full_name, email, password, isProfileVisible)
+VALUES ('Travis Hampton' , 'travis@gmail.com', 'password', true);
+INSERT INTO users (full_name, email, password, isProfileVisible)
+VALUES ('Verna Saunders' , 'verna@gmail.com', 'password', true);
+INSERT INTO users (full_name, email, password, isProfileVisible)
+VALUES ('Tyler Joseph' , 'tyler@gmail.com', 'password', true);
+INSERT INTO users (full_name, email, password, isProfileVisible)
+
