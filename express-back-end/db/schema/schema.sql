b6d6fdfe360a8bfd4eccdba61c84ed126e2413ca
ban-mazin <ban.qaddoori89@gmail.com>
2021-05-09 04:20:04 +0000

    update schema

--- express-back-end/db/schema/schema.sql
+++ express-back-end/db/schema/schema.sql
@@ -1,11 +1,11 @@
 -- Drop and recreate Users table (Example)
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS user_properties CASCADE;
-DROP TABLE IF EXISTS type CASCADE;
+DROP TABLE IF EXISTS type1 CASCADE;
 DROP TABLE IF EXISTS property_loc_points CASCADE;
 DROP TABLE IF EXISTS users_prefrences CASCADE;
 DROP TABLE IF EXISTS user_searches CASCADE;
-DROP TABLE IF EXISTS users CASCADE;
+
 
 
 
@@ -22,26 +22,30 @@ CREATE TABLE users (
 CREATE TABLE user_properties (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  description TEXT,
+  description TEXT NOT NULL,
   address VARCHAR(255) NOT NULL,
   state BOOLEAN,
   image_url VARCHAR(255),
   isProfileVisible BOOLEAN,
   type VARCHAR(255),
+  UNIQUE (user_id)
 );
 
-CREATE TABLE type (
+CREATE TABLE type1 (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  rental BOOLEAN
+  rental BOOLEAN,
+  user_property_id INTEGER REFERENCES user_properties(id) ON DELETE CASCADE,
+  UNIQUE (user_property_id)
 );
 
 
 CREATE TABLE property_loc_points (
   id SERIAL PRIMARY KEY NOT NULL,
-  property_id INTEGER REFERENCES user_properties(id) ON DELETE CASCADE,
+  user_property_id INTEGER REFERENCES user_properties(id) ON DELETE CASCADE,
   latitude FLOAT NOT NULL,
-  longitude FLOAT NOT NULL
+  longitude FLOAT NOT NULL,
+  UNIQUE (user_property_id)
 );
 
 
@@ -50,10 +54,13 @@ CREATE TABLE users_prefrences(
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   bedroomNo INTEGER NOT NULL,
-  size INTEGER NOT NULL
+  size INTEGER NOT NULL,
+  UNIQUE (user_id)
 );
 
 CREATE TABLE user_searches(
   id SERIAL PRIMARY KEY NOT NULL,
-  description TEXT
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  description TEXT NOT NULL,
+  UNIQUE (user_id)
 );
