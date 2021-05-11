097bc6af4ebece1bbb53c60b3d5f65996c7d5cac
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-05-10 21:29:00 -0700

    Add further functionality and visual changes to the Navbar

--- react-front-end/src/components/Navbar/Navbar.js
+++ react-front-end/src/components/Navbar/Navbar.js
@@ -1,31 +1,37 @@
 import React, { Component } from 'react'
 import { MenuItems } from './MenuItems'
+import { Button } from "../Button"
 import './Navbar.css'
 
 class Navbar extends Component {
-  render() {
-    return(
-      <nav className="NavbarItems">
-        <h1 
-          className="navbar-logo">React<i className="fab fa-react"></i>
-        </h1>
-        <div className="menu-icon">
+  state = { clicked: false }
 
-        </div>
-        <ul>
-          {MenuItems.map((item, index) => {
-            return (
-              <li key={index}>
-                <a className={item.cName} href={item.url}>
-                {item.title}
-                </a>
-              </li>
-            )
-          })}
-        </ul>
-      </nav>
-    ) 
+  handleClick = () => {
+      this.setState({ clicked: !this.state.clicked })
+  }
+
+  render() {
+      return(
+          <nav className="NavbarItems">
+              <h1 className="navbar-logo">React<i className="fab fa-react"></i></h1>
+              <div className="menu-icon" onClick={this.handleClick}>
+                  <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+              </div>
+              <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                  {MenuItems.map((item, index) => {
+                      return (
+                          <li key={index}>
+                              <a className={item.cName} href={item.url}>
+                              {item.title}
+                              </a>
+                          </li>
+                      )
+                  })}
+              </ul>
+              <Button>Sign Up</Button>
+          </nav>
+      )
   }
 }
 
-export default Navbar;
+export default Navbar
