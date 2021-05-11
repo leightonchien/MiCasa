6e2f9384891f2542d3bbdd078634cc1390e3f75e
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-05-10 19:06:27 -0700

    Create the Navbar with materials ui, etc.

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,38 +1,52 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import NavBar from './components/NavBar'
 import './App.css';
 
+
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      message: 'Click the button to load data!'
-    }
-  }
-
-  fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) // The entire response from the Rails API
-
-      console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
-      });
-    }) 
-  }
-
   render() {
     return (
-      <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
-          Fetch Data
-        </button>        
+      <div>
+        <NavBar />
       </div>
     );
   }
 }
 
 export default App;
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       message: 'Click the button to load data!'
+//     }
+//   }
+
+//   fetchData = () => {
+//     axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
+//     .then((response) => {
+//       // handle success
+//       console.log(response.data) // The entire response from the Rails API
+
+//       console.log(response.data.message) // Just the message
+//       this.setState({
+//         message: response.data.message
+//       });
+//     }) 
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>{ this.state.message }</h1>
+//         <button onClick={this.fetchData} >
+//           Fetch Data
+//         </button>        
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
